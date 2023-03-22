559c89237c6e1768e3488c7f19a36f99c612d1e0
Anoname112 <44809772+Anoname112@users.noreply.github.com>
2023-03-22 11:43:12 +0700

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,1 +1,1 @@
-var excluded = ['num', 'tier', 'battleOnly', 'evos', 'prevo', 'evoLevel', 'evoItem', 'evoType', 'evoCondition', 'requiredItem', 'requiredMove', 'otherFormes', 'formeOrder', 'changesFrom'];
+const excluded = ['num', 'tier', 'battleOnly', 'evos', 'prevo', 'evoLevel', 'evoItem', 'evoType', 'evoCondition', 'requiredItem', 'requiredMove', 'otherFormes', 'formeOrder', 'changesFrom'];
